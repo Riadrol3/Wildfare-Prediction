b30f3c7592ab17ef35cc9ee149de4f1c34ed95be
Riadrol3 <120026398+Riadrol3@users.noreply.github.com>
2024-12-08 21:09:05 +0100

    database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Database configuration
-DATABASE_URL = "postgresql+asyncpg://username:password@localhost/dbname" 
+DATABASE_URL = "postgresql+asyncpg://postgres:Riad108515@localhost:5432/Agile_project"
 
 # Create the database engine
 engine = create_async_engine(DATABASE_URL, echo=True)
